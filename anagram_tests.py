c48e08a0d413c89319a501a24217b4459c253b6d
Carlie Copeland <carliecope@gmail.com>
2016-06-18 14:20:26 -0500

    add unit tests, input checks and cli

--- anagram_tests.py
+++ anagram_tests.py
@@ -1,1 +1,38 @@
-"""Testing for the solve_anagram function"""
+"""
+Testing for the anagram solver and input functions
+"""
+import unittest
+from solve_anagram import solve_anagram
+from solve_anagram import validateInput 
+from solve_anagram import askUser 
+
+class SolveAnagramTests(unittest.TestCase):
+    
+    def test_string_NOT_in_dict(self):
+        isNone = solve_anagram("antidisestablishmentarianism")
+        self.assertEqual(isNone, None)
+
+    def test_string_in_dict(self):
+    	anagram_list = solve_anagram("rates")
+    	self.assertListEqual(anagram_list, ['tares', 'tears', 'aster', 'stare'])
+    
+    def test_NO_valid_anagrams(self):
+        noneFound = solve_anagram("foul")
+        self.assertEqual(noneFound, None)
+
+class ValidateInputTests(unittest.TestCase):
+
+    def test_numeric_input(self):
+    	numericInput = validateInput(9999)
+    	self.assertEqual(numericInput, None)
+
+    def test_non-alphabetic_input(self):
+    	nonAlphabetInput = validateInput(")(*)*)*&^(*&^(*&^(*&^")
+    	self.assertEqual(nonAlphabetInput, None)
+    
+    def test_max_word_length(self):
+    	maxLengthInput = validateInput("antidisestablishmentarianism")
+    	self.assertEqual(maxLengthInput, None)
+
+if __name__ == "__main__":
+    unittest.main()
--- solve_anagram.py
+++ solve_anagram.py
@@ -1,66 +1,86 @@
-"""Find valid anagrams in a file.
-
-Given a word from user input, check if the word is in the given dictionary file, then generate anagrams of the word and return them to the user. 
 """
-import sys
+Generate anagrams for a given word.
+
+Given a word from user input, check if the word is in our dictionary.txt file, generate anagrams of the word that are in valid US English and return them to the user. 
+"""
+
 from itertools import permutations
+import enchant
 
-def getInput():
-    input_word = input("Enter a word to find its anagrams").strip().lower()
-    print(input_word)
+US_Dict = enchant.Dict("en_US")
 
-    if len(input_word) > 12:
-        print("Looks like that word isn't in out dictionary")
-        input_word = input("Enter a new word").strip().lower() 
-    elif input_word == None:
-        input_word = input("Enter a new word")
-        input_word = input("Enter a word to find its anagrams").strip().lower()
-         
 def solve_anagram(input_word):
-    """Takes input word, checks for it in dictionary, then outputs a list of anagrams"""
+    """Checks for word in dictionary and if found, outputs a list of anagrams"""
+    
     raw_perm_list = []
     valid_perm_list = []
     
     # Load dictionary.txt file into a dictionary in memory
-    max_len = 0
     dictionary = {}
-
     with open('./dictionary.txt', 'r', encoding="utf-8") as handle:
         for line in handle:
             line = line.rstrip()  # remove '\n' at end of line
             dictionary[line] = True
-            if len(line) > max_len:
-                max_len = len(line)
-        print(max_len)
-
-    # Ask for intput
-    # my_input = input("Enter something, yo!")
 
     # Check in input word in dictionary
     inDictionary = dictionary.get(input_word, None)
     if inDictionary is not None:
         print("Looks like {} is in our dictionary.".format(input_word))
 
-        # Generate permutations of input_word
+        # Generate raw permutations of input_word
         for i in list(permutations(input_word)):
-            raw_perm_list.append("".join(i))
-        # print(raw_perm_list)
+            raw_perm = "".join(i)
+            if raw_perm not in raw_perm_list and raw_perm != input_word:
+                raw_perm_list.append(raw_perm) 
 
-        # Check and store which permutations are in dictionary
+        # Check for permutations in dictionary
         for perm in raw_perm_list:
-            if dictionary.get(perm) != None:
-                valid_perm_list.append(perm)
-
-        print(valid_perm_list)
+            if dictionary.get(perm) != None and US_Dict.check(perm) != False:
+                valid_perm_list.append(perm) 
 
         # Sort permutations by 2nd letter
         sortedList = sorted(valid_perm_list, key=lambda n: n[1:])
-        print(sortedList)
-        return sortedList
+
+        if len(sortedList) != 0:
+            print("The anagrams of {} are: {}".format(input_word, sortedList))
+            return sortedList
+        else:
+            print("However, there are no anagrams for {}".format(input_word))
+            return None
     else:
-        print("Looks like {} isn't in out dictionary.".format(input_word))
-   
+        print("Looks like {} isn't in our dictionary.".format(input_word))
+        return None
+
+def validateInput(input_word):
+    """Checks for valid input words"""
+
+    if not input_word.isalpha():
+        return 
+    elif len(input_word) > 12:
+        print("Looks like {} isn't in our dictionary.".format(input_word))
+        return 
+    elif input_word == None:
+        return
+    elif US_Dict.check(input_word) == False:
+        return 
+    else:
+        return True
+        
+def askUser():
+    """Takes input word and calls the solve_anagram function"""
+
+    isValid = None
+
+    while isValid == None:
+        input_word = input("Enter a word to find its anagrams: ")
+        input_word.strip().lower()
+        print(input_word)
+    
+        isValid = validateInput(input_word)
+
+    solve_anagram(input_word)
+
 if __name__ == '__main__':
-    getInput()
+    askUser()
 
 
