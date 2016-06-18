f3a155073d8a8d3b0db644d2bec2dda721cec379
Carlie Copeland <carliecope@gmail.com>
2016-06-18 16:47:24 -0500

    add unit testsand edit comments in solver script

--- anagram_tests.py
+++ anagram_tests.py
@@ -1,32 +1,35 @@
 """
-Testing for the anagram solver and input functions
+Testing for the anagram solver and input function
 """
 import unittest
 from solve_anagram import solve_anagram
 from solve_anagram import validateInput 
-from solve_anagram import askUser 
 
 class SolveAnagramTests(unittest.TestCase):
     
     def test_string_NOT_in_dict(self):
-        isNone = solve_anagram("antidisestablishmentarianism")
-        self.assertEqual(isNone, None)
+        output = solve_anagram("antidisestablishmentarianism")
+        self.assertEqual(output, None)
 
     def test_string_in_dict(self):
-    	anagram_list = solve_anagram("rates")
-    	self.assertListEqual(anagram_list, ['tares', 'tears', 'aster', 'stare'])
+    	output = solve_anagram("rates")
+    	self.assertListEqual(output, ['tares', 'tears', 'aster', 'stare'])
     
     def test_NO_valid_anagrams(self):
-        noneFound = solve_anagram("foul")
-        self.assertEqual(noneFound, None)
+        output = solve_anagram("foul")
+        self.assertEqual(output, None)
+
+    def test_alphabetical_by_2nd_letter(self):
+    	output = solve_anagram("takes")
+    	self.assertListEqual(output, ['skate', 'stake', 'steak'])
 
 class ValidateInputTests(unittest.TestCase):
 
-    def test_numeric_input(self):
-    	numericInput = validateInput(9999)
-    	self.assertEqual(numericInput, None)
+    def test_NON_string_input(self):
+    	with self.assertRaises(AttributeError):
+    	    nonStringInput = validateInput(9999)
 
-    def test_non-alphabetic_input(self):
+    def test_NON_alphabetic_string_input(self):
     	nonAlphabetInput = validateInput(")(*)*)*&^(*&^(*&^(*&^")
     	self.assertEqual(nonAlphabetInput, None)
     
--- solve_anagram.py
+++ solve_anagram.py
@@ -1,39 +1,39 @@
 """
 Generate anagrams for a given word.
 
-Given a word from user input, check if the word is in our dictionary.txt file, generate anagrams of the word that are in valid US English and return them to the user. 
+Given a word from user input, check if the word is in our dictionary.txt file, generate anagrams of the word that are both in our dictionary AND are valid US English and return them to the user. 
 """
 
-from itertools import permutations
+import itertools
 import enchant
 
 US_Dict = enchant.Dict("en_US")
 
 def solve_anagram(input_word):
-    """Checks for word in dictionary and if found, outputs a list of anagrams"""
+    """Checks for word in dictionary and if found, outputs a list of valid anagrams"""
     
     raw_perm_list = []
     valid_perm_list = []
     
-    # Load dictionary.txt file into a dictionary in memory
+    # Load dictionary.txt file into dictionary in memory
     dictionary = {}
     with open('./dictionary.txt', 'r', encoding="utf-8") as handle:
         for line in handle:
             line = line.rstrip()  # remove '\n' at end of line
             dictionary[line] = True
 
-    # Check in input word in dictionary
+    # Check for input word in dictionary.txt
     inDictionary = dictionary.get(input_word, None)
     if inDictionary is not None:
         print("Looks like {} is in our dictionary.".format(input_word))
 
-        # Generate raw permutations of input_word
-        for i in list(permutations(input_word)):
+        # Generate raw permutations of input_word, stops after 5000 permutations
+        for i in list(itertools.islice(itertools.permutations(input_word), 5000)):
             raw_perm = "".join(i)
             if raw_perm not in raw_perm_list and raw_perm != input_word:
                 raw_perm_list.append(raw_perm) 
 
-        # Check for permutations in dictionary
+        # Check permutations against our dictionary and US English Dictionary
         for perm in raw_perm_list:
             if dictionary.get(perm) != None and US_Dict.check(perm) != False:
                 valid_perm_list.append(perm) 
@@ -73,9 +73,9 @@ def askUser():
 
     while isValid == None:
         input_word = input("Enter a word to find its anagrams: ")
-        input_word.strip().lower()
-        print(input_word)
-    
+        input_word = input_word.strip()
+        input_word = input_word.lower()
+  
         isValid = validateInput(input_word)
 
     solve_anagram(input_word)
