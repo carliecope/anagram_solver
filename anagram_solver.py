14a421caa1142aa5959693271174863977d7cfd7
Carlie Copeland <carliecope@gmail.com>
2016-06-19 15:19:55 -0500

    optimize for single or multiple word input, tests and run fuction updated accordingly

--- anagram_solver.py
+++ anagram_solver.py
@@ -1,13 +1,15 @@
 """
-Generate anagrams for a given word.
+Generates anagrams from user input words. 
 
-Given a word from user input, check if the word is in our dictionary.txt file, generate anagrams of the word that are in our dictionary and return them to the user. 
+Options optimized for single and multiple user inputs. 
+
+Given a word from user input, checks if the word is in our dictionary.txt file. If found, generates anagrams of the word from our dictionary and returns them to the user. 
 """
 import sys
 from collections import defaultdict
 
 def parse_dictionary(filename):
-   """Reads file and makes dict of sorted words"""
+   """Reads file and makes dictionary of sorted words"""
    word_anagrams = defaultdict(set)
 
    with open(filename) as dictionary:
@@ -31,7 +33,7 @@ def validateInput(input_word):
       return True
         
 def askUser():
-   """Takes input and calls validation funct"""
+   """Takes input and calls validation function"""
 
    isValid = None
 
@@ -42,9 +44,9 @@ def askUser():
 
       isValid = validateInput(input_word)
 
-   return input_word
+   return input_word  
 
-def main(input_word, word_anagrams):
+def solveMultiple(input_word, word_anagrams):
    """Checks for word in dictionary and if found, outputs a list of valid anagrams"""
    
    valid_anagram = []
@@ -62,22 +64,54 @@ def main(input_word, word_anagrams):
          return valid_anagram
       else:
          print("There no valid anagrams for {} in our dictionary.".format(input_word))
-         return None
+
    else:
       print("No matches for {} found in our dictionary.".format(input_word))
-      return None
+
+def solveOne(input_word):
+   """Checks for word in dictionary and if found, outputs a list of valid anagrams"""
+   
+   valid_anagram = []
+
+   with open("./dictionary.txt") as dictionary:
+      for word in dictionary:
+         word = word.rstrip()
+         if sorted(list(input_word)) == sorted(list(word)):
+            valid_anagram.append(word)
+
+   valid_anagram = sorted(valid_anagram, key=lambda n: n[1:])          
+   
+   if len(valid_anagram) > 1:
+      print("The anagrams for {} are: {}".format(input_word, valid_anagram))
+      return valid_anagram
+   elif len(valid_anagram) == 1:
+      print("There no valid anagrams for {} in our dictionary.".format(input_word))
+   else:
+      print("No matches for {} found in our dictionary.".format(input_word))
 
 def run():
    """Runs CLI until user opts to exit"""
-   word_anagrams = parse_dictionary("dictionary.txt")
 
-   while True:
+   inputCount = input("Would you like to solve for more than one word? y or n: ")
+
+   if inputCount == "n":
       input_word = askUser()
-      main(input_word, word_anagrams)
-      replay_feedback = input("Enter q to quit or press enter to keep playing: ")
+      solveOne(input_word)
 
-      if replay_feedback == "q":
-         sys.exit()
+   elif inputCount == "y":
+      word_anagrams = parse_dictionary("dictionary.txt")
+      run = True
+
+      while run == True:
+         input_word = askUser()
+         solveMultiple(input_word, word_anagrams)
+         replay_feedback = input("Enter q to quit or press enter to keep playing: ")
+
+         if replay_feedback == "q":
+            run = False 
+      sys.exit()
+   else: 
+      sys.exit() 
 
 if __name__ == "__main__":
    run()
--- anagram_tests.py
+++ anagram_tests.py
@@ -4,30 +4,47 @@ Testing for the anagram solver and input function
 import unittest
 import anagram_solver
 
-# Load dictionary for use in tests of anagram_solver.main()
+# Load dictionary for use in tests of anagram_solver.solveMultiple()
 dictionary = anagram_solver.parse_dictionary("dictionary.txt")
 
-class MainTests(unittest.TestCase):
+class SolveMultipleTests(unittest.TestCase):
     
     def test_string_NOT_in_dict(self):
-        output = anagram_solver.main("antidisestablishmentarianism", dictionary)
+        output = anagram_solver.solveMultiple("antidisestablishmentarianism", dictionary)
         self.assertEqual(output, None)
 
     def test_string_in_dict(self):
-    	output = anagram_solver.main("rates", dictionary)
-    	self.assertListEqual(output, ['tares', 'tarse', 'sater', 'rates', 'tears', 'teras', 'serta', 'tresa', 'artes', 'aster', 'stare', 'strae'])
+    	output = anagram_solver.solveMultiple("rates", dictionary)
+    	self.assertEqual(output, ['tares', 'tarse', 'sater', 'rates', 'tears', 'teras', 'serta', 'tresa', 'artes', 'aster', 'stare', 'strae'])
     
     def test_NO_valid_anagrams(self):
-        output = anagram_solver.main("foul", dictionary)
+        output = anagram_solver.solveMultiple("foul", dictionary)
         self.assertEqual(output, None)
 
     def test_alphabetical_by_2nd_letter(self):
-    	output = anagram_solver.main("takes", dictionary)
-    	self.assertListEqual(output, ['takes', 'keast', 'skate', 'stake', 'steak'])
+    	output = anagram_solver.solveMultiple("takes", dictionary)
+    	self.assertEqual(output, ['takes', 'keast', 'skate', 'stake', 'steak'])
 
     def test_NO_2nd_arg(self):
         with self.assertRaises(TypeError):
-            output = anagram_solver.main("argument")
+            output = anagram_solver.solveMultiple("argument")
+class SolveOneTests(unittest.TestCase):
+
+    def test_string_NOT_in_dict(self):
+        output = anagram_solver.solveOne("antidisestablishmentarianism")
+        self.assertEqual(output, None)
+
+    def test_string_in_dict(self):
+        output = anagram_solver.solveOne("rates")
+        self.assertEqual(output, ['tares', 'tarse', 'sater', 'rates', 'tears', 'teras', 'serta', 'tresa', 'artes', 'aster', 'stare', 'strae'])
+    
+    def test_NO_valid_anagrams(self):
+        output = anagram_solver.solveOne("foul")
+        self.assertEqual(output, None)
+
+    def test_alphabetical_by_2nd_letter(self):
+        output = anagram_solver.solveOne("takes")
+        self.assertEqual(output, ['takes', 'keast', 'skate', 'stake', 'steak'])
 
 class ValidateInputTests(unittest.TestCase):
 
