028a20ee2380799bb55fad7b20cbbef9128f9dc6
Carlie Copeland <carliecope@gmail.com>
2016-06-19 02:24:03 -0500

    eliminate permutations in algo, updated tests accordingly , and updated cli

---
+++ anagram_solver.py
@@ -0,0 +1,84 @@
+"""
+Generate anagrams for a given word.
+
+Given a word from user input, check if the word is in our dictionary.txt file, generate anagrams of the word that are in our dictionary and return them to the user. 
+"""
+import sys
+from collections import defaultdict
+
+def parse_dictionary(filename):
+   """Reads file and makes dict of sorted words"""
+   word_anagrams = defaultdict(set)
+
+   with open(filename) as dictionary:
+      for word in dictionary:
+         word = word.rstrip()
+         word_anagrams[''.join(sorted(word))].add(word)
+
+   return word_anagrams
+
+def validateInput(input_word):
+   """Checks for valid input words"""
+
+   if not input_word.isalpha():
+      return 
+   elif len(input_word) > 12:
+      print("Looks like {} isn't in our dictionary.".format(input_word))
+      return 
+   elif input_word == None:
+      return
+   else:
+      return True
+        
+def askUser():
+   """Takes input and calls validation funct"""
+
+   isValid = None
+
+   while isValid == None:
+      input_word = input("Enter an English word to find its anagrams: ")
+      input_word = input_word.strip()
+      input_word = input_word.lower()
+
+      isValid = validateInput(input_word)
+
+   return input_word
+
+def main(input_word, word_anagrams):
+   """Checks for word in dictionary and if found, outputs a list of valid anagrams"""
+   
+   valid_anagram = []
+
+   sorted_word = ''.join(sorted(input_word))
+
+   if word_anagrams[sorted_word]:
+      for instance in word_anagrams[sorted_word]:
+         valid_anagram.append(instance)
+
+      valid_anagram = sorted(valid_anagram, key=lambda n: n[1:])          
+      
+      if len(valid_anagram) > 1:
+         print("The anagrams for {} are: {}".format(input_word, valid_anagram))
+         return valid_anagram
+      else:
+         print("There no valid anagrams for {} in our dictionary.".format(input_word))
+         return None
+   else:
+      print("No matches for {} found in our dictionary.".format(input_word))
+      return None
+
+def run():
+   word_anagrams = parse_dictionary("dictionary.txt")
+
+   while True:
+      input_word = askUser()
+      main(input_word, word_anagrams)
+      replay_feedback = input("Enter q to quit or press enter to keep playing: ")
+
+      if replay_feedback == "q":
+         sys.exit()
+
+if __name__ == "__main__":
+   run()
+
+
--- anagram_tests.py
+++ anagram_tests.py
@@ -2,40 +2,55 @@
 Testing for the anagram solver and input function
 """
 import unittest
-from solve_anagram import solve_anagram
-from solve_anagram import validateInput 
+import anagram_solver
 
-class SolveAnagramTests(unittest.TestCase):
+# Load dictionary for use in tests of anagram_solver.main()
+dictionary = anagram_solver.parse_dictionary("dictionary.txt")
+
+class MainTests(unittest.TestCase):
     
     def test_string_NOT_in_dict(self):
-        output = solve_anagram("antidisestablishmentarianism")
+        output = anagram_solver.main("antidisestablishmentarianism", dictionary)
         self.assertEqual(output, None)
 
     def test_string_in_dict(self):
-    	output = solve_anagram("rates")
-    	self.assertListEqual(output, ['tares', 'tears', 'aster', 'stare'])
+    	output = anagram_solver.main("rates", dictionary)
+    	self.assertListEqual(output, ['tares', 'tarse', 'sater', 'rates', 'tears', 'teras', 'serta', 'tresa', 'artes', 'aster', 'stare', 'strae'])
     
     def test_NO_valid_anagrams(self):
-        output = solve_anagram("foul")
+        output = anagram_solver.main("foul", dictionary)
         self.assertEqual(output, None)
 
     def test_alphabetical_by_2nd_letter(self):
-    	output = solve_anagram("takes")
-    	self.assertListEqual(output, ['skate', 'stake', 'steak'])
+    	output = anagram_solver.main("takes", dictionary)
+    	self.assertListEqual(output, ['takes', 'keast', 'skate', 'stake', 'steak'])
+
+    def test_NO_2nd_arg(self):
+        with self.assertRaises(TypeError):
+            output = anagram_solver.main("argument")
 
 class ValidateInputTests(unittest.TestCase):
 
     def test_NON_string_input(self):
     	with self.assertRaises(AttributeError):
-    	    nonStringInput = validateInput(9999)
+    	    output = anagram_solver.validateInput(9999)
 
     def test_NON_alphabetic_string_input(self):
-    	nonAlphabetInput = validateInput(")(*)*)*&^(*&^(*&^(*&^")
-    	self.assertEqual(nonAlphabetInput, None)
+    	output = anagram_solver.validateInput(")(*)*)*&^(*&^(*&^(*&^")
+    	self.assertEqual(output, None)
     
     def test_max_word_length(self):
-    	maxLengthInput = validateInput("antidisestablishmentarianism")
-    	self.assertEqual(maxLengthInput, None)
+    	output = anagram_solver.validateInput("antidisestablishmentarianism")
+    	self.assertEqual(output, None)
+
+class ParseDictionaryTests(unittest.TestCase):
+
+    def test_file_not_found(self):
+        with self.assertRaises(FileNotFoundError):
+            output = anagram_solver.parse_dictionary("random.txt")
 
 if __name__ == "__main__":
     unittest.main()
+
+
+
